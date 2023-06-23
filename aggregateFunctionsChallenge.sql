a591584133a4f2250b318d7ffded03e2710645ed
Will-Andrade <willian.santos.andra@outlook.com>
2023-06-23 14:42:36 -0400

    fix: fixed some solutions and added a new solution to the longest book one

--- aggregateFunctionsChallenge.sql
+++ aggregateFunctionsChallenge.sql
@@ -3,7 +3,7 @@ USE book_shop;
 SELECT COUNT(*) FROM books;
 
 --
-SELECT released_year, COUNT(title) FROM books GROUP BY released_year;
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
 
 --
 SELECT SUM(stock_quantity) FROM books;
@@ -12,9 +12,11 @@ SELECT SUM(stock_quantity) FROM books;
 SELECT author_lname, AVG(released_year) FROM books GROUP BY author_lname;
 
 --
-SELECT CONCAT(author_fname, ' ', author_lname) AS longest_book_author FROM books
-  WHERE pages = (SELECT MAX(pages) FROM books);
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS longest_book_author FROM books
+--   WHERE pages = (SELECT MAX(pages) FROM books);
+SELECT CONCAT(author_fname, ' ',  author_lname) AS author FROM books 
+  ORDER BY pages DESC LIMIT 1;
 
 --
-SELECT released_year AS year, COUNT(title) AS '# books', AVG(pages) AS 'avg pages'
-  FROM BOOKS GROUP BY year;
+SELECT released_year AS year, COUNT(*) AS '# books', AVG(pages) AS 'avg pages'
+  FROM books GROUP BY year ORDER BY year;
